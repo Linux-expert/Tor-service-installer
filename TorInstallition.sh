1019ba951c997442c29153614aff5de216687571
Roham <49027977+Linux-expert@users.noreply.github.com>
2019-04-03 10:42:52 +0000

    Add files via upload

--- TorInstallition.sh
+++ TorInstallition.sh
@@ -13,7 +13,9 @@ if [ "$tor_socks" == "y" ];then
     read -p "Do you want to install tor browser? (size : 71.7 mb) (y/n) : " tor_browser
 elif [ "$tor_browser" == "y" ];then
     sudo torsocks wget https://dist.torproject.org/torbrowser/8.0.8/tor-browser-linux64-8.0.8_en-US.tar.xz
-    sudo pacman -U tor-browser-linux64-8.0.8_en-US.tar.xz
+    cd tor-browser_en-US
+    chmod +x 'Tor Browser Setup.desktop'
+    ./'Tor Browser Setup.desktop'
     echo "Tor browser installed !"
     echo "Good bye !"
 exit
